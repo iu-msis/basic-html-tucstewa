2fc0d4d29fa4521a713f7007eec5334c520fc4de
Tucker Stewart <tucstewa@iu.edu>
2019-09-15 20:56:31 -0400

    Pages now linked. Email is now clickable.

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -35,9 +35,7 @@ fetchPatients() {
 fetch('https://randomuser.me/api/?nat')
 .then(response=>response.json())
 .then(json=>{waitingApp.patients=json.results[0]});
-//.then(function(response){ console.log(response.json())});
-//.then(function(response) {return response.json()})
-//.then(function(json) {waitingApp.people = json});
+
 
 }
 },
